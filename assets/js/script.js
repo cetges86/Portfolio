fc5d25f12d0ab812a4fcb25e6afbe9d089d66c82
Christopher Etges <c.etges86@gmail.com>
2018-04-15 17:33:54 -0600

    added portfolio images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
-    
+
+    $('#headshot').css('opacity', 0);
+
+    $("#headshot").waypoint(function () {
+        $('#headshot').addClass('fadeInLeft');
+    }
+        , { offset: '50%' });
+
+
+    $('#bioP').css('opacity', 0);
+    $("#bioP").waypoint(function () {
+        $('#bioP').addClass('fadeInRight');
+    }
+        , { offset: '50%' });
 })
