c29146d1dc6f6c2045336e87cdcf49344c3c248f
Christopher Etges <c.etges86@gmail.com>
2018-07-02 21:53:37 -0600

    fix js, add project

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,83 +36,15 @@ $(document).ready(function () {
     }
         , { offset: '50%' });
 
-
-
-    $('#proj1').css('opacity', 0);
-    $('#proj1').waypoint(function () {
-        $('#proj1').addClass('fadeInUp')
+    for (let i = 0; i < 14; i++) {
+        $(`#proj${i}`).css('opacity', 0);
+        $(`#proj${i}`).waypoint(function () {
+            $(`#proj${i}`).addClass('fadeInUp')
+            $(`#proj${i}`).css('opacity', 1);
+        }
+            , { offset: '40%' });
     }
-        , { offset: '40%' });
 
-    $('#proj2').css('opacity', 0);
-    $('#proj2').waypoint(function () {
-        $('#proj2').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-
-    $('#proj3').css('opacity', 0);
-    $('#proj3').waypoint(function () {
-        $('#proj3').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-    $('#proj4').css('opacity', 0);
-    $('#proj4').waypoint(function () {
-        $('#proj4').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-    $('#proj5').css('opacity', 0);
-    $('#proj5').waypoint(function () {
-        $('#proj5').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-    $('#proj6').css('opacity', 0);
-    $('#proj6').waypoint(function () {
-        $('#proj6').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-    $('#proj7').css('opacity', 0);
-    $('#proj7').waypoint(function () {
-        $('#proj7').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-    $('#proj8').css('opacity', 0);
-    $('#proj8').waypoint(function () {
-        $('#proj8').addClass('fadeInUp')
-    }
-        , { offset: '40%' });
-
-    $('#proj9').css('opacity', 0);
-    $('#proj9').waypoint(function () {
-        $('#proj9').addClass('fadeInUp')
-    }
-        , { offset: '50%' });
-
-    $('#proj10').css('opacity', 0);
-    $('#proj10').waypoint(function () {
-        $('#proj10').addClass('fadeInUp')
-    }
-        , { offset: '50%' });
-
-    $('#proj11').css('opacity', 0);
-    $('#proj11').waypoint(function () {
-        $('#proj11').addClass('fadeInUp')
-    }
-        , { offset: '50%' });
-
-    $('#proj12').css('opacity', 0);
-    $('#proj12').waypoint(function () {
-        $('#proj12').addClass('fadeInUp')
-
-    }
-        , { offset: '50%' });
-
-    
     $('.btn-large').waypoint(function () {
         $('.btn-large').addClass('pulse')
     }
