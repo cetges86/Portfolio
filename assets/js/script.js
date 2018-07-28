29936d18c70682fd6d196ecdc4c4614095319f3c
Christopher Etges <c.etges86@gmail.com>
2018-07-28 16:53:55 -0600

    v2.0 complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
+    // activate materialize js functions
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
     $('.carousel').carousel();
 
+    // function to advance carousel automatically
+    advanceCarousel = () => {
+        $('.carousel').carousel('next');
+    }
 
-
-
+    // set initial opacity on animated elements
     $('#headshot').css('opacity', 0);
     $('#bioP').css('opacity', 0);
     $('#section-header').css('opacity', 0);
     $('.carousel').css('opacity', 0);
     $('#down-arrow').css('opacity', 0);
 
+    // animation trigger points
     $("#down-arrow").waypoint(function () {
         $('#down-arrow').addClass('bounceInDown');
         $('#down-arrow').css('opacity', 1);
@@ -33,29 +38,25 @@ $(document).ready(function () {
     }
         , { offset: '50%' });
 
-    advanceCarousel = () => {
-        $('.carousel').carousel('next');
-    }
-
     $('.carousel').waypoint(function () {
         $('.carousel').addClass('fadeIn');
-        setInterval(advanceCarousel, 1500);
+        setInterval(advanceCarousel, 2000);
     }
         , { offset: '50%' });
-
-    for (let i = 0; i < 14; i++) {
+    // for loop to run through all project cards
+    for (let i = 0; i < 15; i++) {
         $(`#proj${i}`).css('opacity', 0);
         $(`#proj${i}`).waypoint(function () {
             $(`#proj${i}`).addClass('fadeInUp')
             $(`#proj${i}`).css('opacity', 1);
         }
-            , { offset: '40%' });
+            , { offset: '60%' });
     }
 
     $('.btn-large').waypoint(function () {
-        $('.btn-large').addClass('pulse')
+        $('.btn-large').addClass('slideInUp')
     }
         , { offset: '90%' });
 
 
-})
+});
