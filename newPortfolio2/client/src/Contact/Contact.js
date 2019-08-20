ef876da57bf54a3b3c93e05b0cb064f98a331784
Christopher Etges <c.etges86@gmail.com>
2019-08-19 22:29:14 -0600

    finishing up desktop view

--- newPortfolio2/client/src/Contact/Contact.js
+++ newPortfolio2/client/src/Contact/Contact.js
@@ -2,18 +2,25 @@ import React from 'react';
 import "../main.css";
 import githubLogo from "../images/githublogo.png"
 import linkedInLogo from "../images/linkedinlogo.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhone, faDownload } from '@fortawesome/free-solid-svg-icons';
 import resumeFile from "../files/ChrisEtgesResume2019.pdf";
 
 const Contact = () => {
+    const emailIcon = <FontAwesomeIcon icon ={faEnvelope} />
+    const phoneIcon = <FontAwesomeIcon icon ={faPhone} />
+    const downloadIcon = <FontAwesomeIcon icon ={faDownload} />
+
+
     return (
         <div id="contact">
             <h2 className="subheading center">Get In Touch</h2>
             <br /><br />
             <div className="contact-buttons">
                 <div></div>
-                <div><a className="contact-links" href="mailto:c.etges86@gmail.com" rel="noopener noreferrer" target="_blank">Email</a></div>
-                <div><span className="contact-links">Call  303.518.2933</span> </div>
-                <div><a className="contact-links" href={resumeFile} download >Resume</a></div>
+                <div><a className="contact-links" href="mailto:c.etges86@gmail.com" rel="noopener noreferrer" target="_blank">{emailIcon} Email</a></div>
+                <div><span className="contact-links">{phoneIcon} Call  303.518.2933</span> </div>
+                <div><a className="contact-links" href={resumeFile} download >{downloadIcon} Resume</a></div>
                 <div></div>
             </div>
             <br/>
