599d5e2e175d18ac9facbeb1728916a9b35fc9cb
cetges86 <c.etges86@gmail.com>
2019-11-06 16:05:09 -0700

    css styling

--- newPortfolio2/client/src/Components/Project.js
+++ newPortfolio2/client/src/Components/Project.js
@@ -3,29 +3,30 @@ import "../main.css";
 
 const Project = (props) => {
     return (
-    <div className="project-card">
-        <div className="card-content">
-            <h2 className="project-title center">{props.title}</h2>
-            <br />
-            <img className="project-thumb" alt={props.title} src={props.image}></img>
-            <br />
-            <br />
-            <p className="project-desc">{props.description}</p>
-            <br />
-            <div className="col-3-wrapper" id="mobile-grid">
-                <a className="project-links" rel="noopener noreferrer" target="_blank" href={props.github_link}>Github Link</a>
-                <h4 className="tech-title">Tech Used:</h4>
-                <a className="project-links" rel="noopener noreferrer" target="_blank" href={props.live_link}>Project Link</a>
-            </div>
+        <div className="project-card">
+            <div className="card-content">
+                <h2 className="project-title center">{props.title}</h2>
+                <br />
+                <div className="grid-wrapper-card">
+                    <img className="project-thumb" alt={props.title} src={props.image}></img>
+                    
+                    <p className="project-desc">{props.description}</p>
+                </div>
+                <br />
+                <div className="col-3-wrapper" id="mobile-grid">
+                    <a className="project-links" rel="noopener noreferrer" target="_blank" href={props.github_link}>Github Link</a>
+                    <h4 className="tech-title">Tech Used:</h4>
+                    <a className="project-links" rel="noopener noreferrer" target="_blank" href={props.live_link}>Project Link</a>
+                </div>
 
-            <ul className="list-wrapper">
-                {props.tech.map((tech) =>
-                    <li key={tech.toString()} className="tech-list">{tech}</li>
-                )
-                }
-            </ul>
+                <ul className="list-wrapper">
+                    {props.tech.map((tech) =>
+                        <li key={tech.toString()} className="tech-list">{tech}</li>
+                    )
+                    }
+                </ul>
+            </div>
         </div>
-    </div>
     )
 }
 
